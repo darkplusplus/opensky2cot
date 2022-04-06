42e75d86b52bc2ad0bca0085808e5a83a9b0ae3a
Greg Buehler <gregory.r.buehler@gmail.com>
2022-04-06 08:20:31 -0700

    fixes to build process

--- scripts/release.sh
+++ scripts/release.sh
@@ -17,14 +17,14 @@ dotnet clean
 
 # win10-x64
 ARCH=win10-x64
-dotnet publish opensky2cot -c Release -r $ARCH --self-contained=true -p:PublishSingleFile=true
+dotnet publish opensky2cot.sln -c Release -r $ARCH --self-contained=true -p:PublishSingleFile=true
 pushd ./opensky2cot/bin/Release/net5.0/$ARCH/publish/
 zip -r $DIST_DIR/opensky2cot-$VERSION-$ARCH.zip .
 popd
 
 # linux-x64
 ARCH=linux-x64
-dotnet publish opensky2cot -c Release -r $ARCH --self-contained=true -p:PublishSingleFile=true
+dotnet publish opensky2cot.sln -c Release -r $ARCH --self-contained=true -p:PublishSingleFile=true
 pushd ./opensky2cot/bin/Release/net5.0/$ARCH/publish/
 tar -czvf $DIST_DIR/opensky2cot-$VERSION-$ARCH.tar.gz .
 popd
